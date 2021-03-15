ae3e8553a786c520115e1c95792b262f22afa5e5
JAKUB BROZ <Broz.Jk@gmail.com>
2021-03-15 14:54:41 +0100

    Update

--- Magic_OOP_methods/New_OOP.py
+++ Magic_OOP_methods/New_OOP.py
@@ -1,5 +1,15 @@
 class complex_number:
 
-    def __init__(self,real_part,im_part):
+    def __init__(self, real_part, im_part):
         self.real_part = real_part
         self.im_part = im_part
+
+    def __add__(self, other):
+        real = self.real_part + other.real_part
+        imaginary = self.im_part + other.im_part
+        return complex_number(real, imaginary)
+
+    def __iadd__(self, other):
+        self.real_part += other.real_part
+        self.im_part += other.im_part
+        return self
